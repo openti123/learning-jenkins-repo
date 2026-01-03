b09cf04d987a0a74753b4f3e42d668a59b040da7
Kwadwo Odei-Lartey <38427568+openti123@users.noreply.github.com>
2026-01-03 13:51:08 +0100

    Add files via upload

---
+++ terraform-config/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket       = "tf-state-bucket-openti"
+    key          = "ec2/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
